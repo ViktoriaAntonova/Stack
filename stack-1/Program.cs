3e1eb56e34a201687b03af82a3e25883eebdd9b0
MEGA <MEGA@gmail.com>
2025-03-17 18:39:16 +0200

    Add project files.

---
+++ stack-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] chars = text.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                stack.Push(chars[i]);
+            }
+            foreach (char c in stack)
+            {
+                Console.Write(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
